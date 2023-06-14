f41cb54941a2fc2ef021d04bf2991c325dd6a52d
Ivan <crueldog50@gmail.com>
2023-06-14 12:20:12 +1000

    task 56

--- Program.cs
+++ Program.cs
@@ -49,6 +49,26 @@ void SortMatrix(int[,] array2d)
     }
 }
 
+void MinValueStr(int[,] array)
+{
+    int min = 0;
+    int str = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (min == 0 || sum < min)
+            {
+                min = sum;
+                str = i + 1;
+            }
+    }
+    Console.WriteLine($"Наименьшая сумма элементов находится в строке {str}.");
+}
+
 Console.WriteLine("Задача 54");
 // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
@@ -65,3 +85,19 @@ PrintMatrix(matrix);
 Console.WriteLine();
 SortMatrix(matrix);
 PrintMatrix(matrix);
+Console.WriteLine();
+
+Console.WriteLine("Задача 56");
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int [,] matrix2 = FillMatrix(EnterNumber("Введите колличество строк: "), EnterNumber("Введите количество столбцов: "));
+PrintMatrix(matrix2);
+MinValueStr(matrix2);
+Console.WriteLine();
